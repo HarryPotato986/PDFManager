7336628078f256e4677a0affae2707c3d86ea798
HarryPotato986 <gdude986@gmail.com>
2025-10-03 22:47:50 -0400

    init commit

---
+++ src/main/java/com/github/HarryPotato986/Main.java
@@ -0,0 +1,11 @@
+package com.github.HarryPotato986;
+
+import javax.swing.*;
+
+public class Main {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("PDF Manager");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+    }
+}
